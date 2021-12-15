a693b691842f727002b97c9a9f7e3859edec34ff
Robson <robsoncezariosilva@gmail.com>
2021-12-15 00:06:19 -0300

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,23 +8,20 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract NFT is ERC721, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-		address private marketplaceAddress; 
+    address private marketplaceAddress;
 
     constructor(address _marketplaceAddress) ERC721("CollectibleNFT", "NFT") {
-				marketplaceAddress = _marketplaceAddress;
-		}
+        marketplaceAddress = _marketplaceAddress;
+    }
 
-    function mint(string memory _tokenURI)
-        public
-        returns (uint256)
-    {
+    function mint(string memory _tokenURI) public returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
-				//_mint(account, newItemId);
+        //_mint(account, newItemId);
         _setTokenURI(newItemId, _tokenURI);
-				setApprovalForAll(marketplaceAddress, true);
+        setApprovalForAll(marketplaceAddress, true);
 
         return newItemId;
     }
@@ -45,37 +42,28 @@ contract NFT is ERC721, ERC721URIStorage {
         return super.tokenURI(tokenId);
     }
 
-    function _baseURI() 
-			internal 
-			view
-			override 
-			returns (string memory) 
-		{
+    function _baseURI() internal view override returns (string memory) {
         return "http://localhost:5000/static/";
     }
 
-		function getAllTokens() 
-			public 
-			view 
-			returns (uint256[] memory) 
-		{
-				uint256 tokenCount = 0;
-				uint256 numTokens = _tokenIds.current();
+    function getAllTokens() public view returns (uint256[] memory) {
+        uint256 tokenCount = 0;
+        uint256 numTokens = _tokenIds.current();
 
-        for (uint tokenId = 1; tokenId <= numTokens; tokenId++) {
-					if (ownerOf(tokenId) == msg.sender) {
-						tokenCount++;
-					}
+        for (uint256 tokenId = 1; tokenId <= numTokens; tokenId++) {
+            if (ownerOf(tokenId) == msg.sender) {
+                tokenCount++;
+            }
         }
-        
-				uint256 index = 0;
+
+        uint256 index = 0;
         uint256[] memory tokenList = new uint256[](tokenCount);
 
-				for (uint tokenId = 1; tokenId <= numTokens; tokenId++) {
-					if (ownerOf(tokenId) == msg.sender) {
-						tokenList[index] = tokenId;
-						index++;
-					}
+        for (uint256 tokenId = 1; tokenId <= numTokens; tokenId++) {
+            if (ownerOf(tokenId) == msg.sender) {
+                tokenList[index] = tokenId;
+                index++;
+            }
         }
 
         return tokenList;
